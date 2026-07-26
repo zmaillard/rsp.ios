3f51ab8320be0dac410db1cd5c9e3005c0b689e4
Zachary Maillard <zmaillard@protonmail.com>
2026-07-26 14:19:06 -0600

    Initial Commit

---
+++ rpbrowser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rpbrowser
+//
+//  Created by Zach Maillard on 7/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rpbrowser/rpbrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  rpbrowserApp.swift
+//  rpbrowser
+//
+//  Created by Zach Maillard on 7/26/26.
+//
+
+import SwiftUI
+
+@main
+struct rpbrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
